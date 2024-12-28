7c88bf2a5119bd0939177520f427fc3a084bd8e4
ManuelFBS <manuelf.borrego@gmail.com>
2024-12-27 22:10:31 -0400

    Eliminación Temporal de "/dist" y otras actualizaciones...

--- Backend/src/routes/cargaPesada.routes.js
+++ Backend/src/routes/cargaPesada.routes.js
@@ -1,46 +1,22 @@
 import { Router } from 'express';
 import {
-  createHeavyLoadForm,
-  // deleteHeavyLoadForm,
-  getAllHeavyLoadForms,
-  getHeavyLoadByFormNumber,
-  getHeavyLoadByFormID,
-  // updateHeavyLoadForm,
+    createHeavyLoadForm,
+    // deleteHeavyLoadForm,
+    getAllHeavyLoadForms,
+    getHeavyLoadByFormNumber,
+    getHeavyLoadByFormID,
+    // updateHeavyLoadForm,
 } from '../controllers/cargaPesada.controller.js';
 import { TokenValidation } from '../authentication/tokens/verifyToken.js';
 
 const router = Router();
 
-router.post(
-  '/addplanillacarga',
-  TokenValidation,
-  createHeavyLoadForm,
-);
+router.post('/addplanillacarga', TokenValidation, createHeavyLoadForm);
 
 router.get('/', TokenValidation, getAllHeavyLoadForms);
 
-router.get(
-  '/planilla/:n_planilla',
-  TokenValidation,
-  getHeavyLoadByFormNumber,
-);
+router.get('/planilla/:n_planilla', TokenValidation, getHeavyLoadByFormNumber);
 
-router.get(
-  '/planillaid/:_id',
-  TokenValidation,
-  getHeavyLoadByFormID,
-);
-
-// router.patch(
-//   '/planilla/edit/:n_planilla',
-//   TokenValidation,
-//   updateHeavyLoadForm,
-// );
-
-// router.delete(
-//   '/planilla/delete/:n_planilla',
-//   TokenValidation,
-//   deleteHeavyLoadForm,
-// );
+router.get('/planillaid/:_id', TokenValidation, getHeavyLoadByFormID);
 
 export default router;
